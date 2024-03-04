1f6c0e6c7bf62a3095fa263e043eb66be5398062
francescomatteucci <francesco.matteucci.boolean@gmail.com>
2024-03-04 14:42:59 +0100

    ADD: PREZZO VIAGGIO

---
+++ app.js
@@ -0,0 +1,20 @@
+//Il programma dovrà chiedere all’utente il numero di chilometri che vuole percorrere e l’età del passeggero.
+//Il programma dovrà chiedere all’utente il numero di chilometri che vuole percorrere.
+let km = prompt("Inserisci i km totali del viaggio")
+//L’età del passeggero.
+let age = prompt("Inserisci l'età")
+
+// PREZZO DEL BIGLIETTO a 0.21 € / km
+const prezzokm = 0.21 //number
+
+//PREZZO VIAGGIO IN BASE AI KM
+
+let prezzo = km * prezzokm //number
+
+let sconto40 = prezzo * 0.40
+if (age < 18) {
+    prezzo = prezzo * 0.8 //SCONTO 20% PER MINORENNI
+} else if(age >= 65) {
+    prezzo = prezzo * 0.6 //SCONTO 40% PER I 65+ d'età
+}
+console.log("Il prezzo del viaggio è " + Number.parseFloat(prezzo).toFixed(2) + "€")
